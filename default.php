825c07ef6915b55d64ca147a68e36405bc590b36
Shannon Norris <shannon_norris@hotmail.co.uk>
2022-10-31 15:40:08 +0000

    Homepage

--- default.php
+++ default.php
@@ -1,4 +1,67 @@
 <!DOCTYPE html>
 <html>
     <?php include "assets/inc/head.php";?>
+    <body id="home">
+        <div id="header">
+            <h1>Apple Valley<i class="fa fa-pagelines" aria-hidden="true"></i><span>Veterinary Centre</span></h1>
+        </div>
+        <div id="content">
+            <div class="flex-container" id="makeappointment">
+                <div class="image"></div>    
+                <div class="text">
+                    <h1>Book an Appointment</h1>
+                    <p>If you are worried about your pet's health, book an appointment to see one of our veterinary consultants.</p>
+                    <h2>Opening Times</h2>
+                    <ul id="openingtimes">
+                        <li><span>Monday</span>09:00 - 17:00</li>
+                        <li><span>Tuesday</span>09:00 - 17:00</li>
+                        <li><span>Wednesday</span>09:00 - 17:00</li>
+                        <li><span>Thursday</span>09:00 - 17:00</li>
+                        <li><span>Friday</span>09:00 - 17:00</li>
+                    </ul>
+                    <p>Call us on <span>01472 123456</span> to book</p>
+                </div>
+            </div>
+            <div class="flex-container" id="petregistration">
+                <div class="text">
+                    <h1>Register Your Pet</h1>
+                    <p>In order to bring your pets for appointments, you'll first need to <span>register them with us</span>.</p>
+                    <p>This can be done over the phone or by popping in to our veterinary centre.</p>
+                    <p>We are able to treat a wide range of species including:</p>
+                    <ul>
+                        <li>Dogs</li>
+                        <li>Cats</li>
+                        <li>Small Pets (Hamsters, Guinea Pigs, Mice etc.)</li>
+                        <li>Birds</li>
+                        <li>Lizards</li>
+                        <li>Fish</li>
+                    </ul>
+                    <p>If your pet is not listed here, we may still be able to help, so <span>don't hesitate to contact us</span> anyway.</p>
+                </div>
+                <div class="image"></div>
+            </div>
+            <div class="flex-container" id="adoption">
+                <div class="image"></div>
+                <div class="text">
+                    <h1>Pet Adoption</h1>
+                    <p>Looking to adopt a pet?</p>
+                    <p>Apple Valley Veterinary Centre is in contact with various rescue centres operating in Grimsby and the surrounding areas. We'd be happy to put you in touch with them to <span>adopt a new furry, feather or scaly friend!</span></p>
+                    <p>We work with the following:</p>
+                    <ul>
+                        <li>Blue Cross Rescue</li>
+                        <li>Temporary</li>
+                        <li>Temporary</li>
+                        <li>Temporary</li>
+                        <li>Temporary</li>
+                        <li>Temporary</li>
+                        <li>Temporary</li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+        <div id="footer">
+            <a href="login.php">Staff Portal</a>
+        </div>
+    </body>
 </html>
